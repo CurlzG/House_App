6521f7a87745e94c06d7bdf2298ca90891f9b7a6
Jordan Gelling <gelljord@gmail.com>
2022-07-21 23:40:36 +1200

    Adding Async Storage and setting up Firebase storage

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,92 @@
 import { StyleSheet,StatusBar } from 'react-native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import database from '@react-native-firebase/database';
+import { firebase } from '@react-native-firebase/database';
 import EditScreenInfo from '../components/EditScreenInfo';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { Text, View } from '../components/Themed';
+import React, {useState,useEffect} from 'react';
 import { RootTabScreenProps } from '../types';
 import Carousel from '../components/Carousel';
+import {apiKey,authDomain,databaseURL,projectId,storageBucket,messagingSenderId,measurementId,appId} from '@env';
 export default function Home({ navigation }: RootTabScreenProps<'Home'>) {
+    // Import the functions you need from the SDKs you need
+
+
+  
+
+    
+    // TODO: Add SDKs for Firebase products that you want to use
+    
+    // https://firebase.google.com/docs/web/setup#available-libraries
+    
+    
+    // Your web app's Firebase configuration
+    
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    
+    const firebaseConfig = {
+      apiKey: apiKey,
+      authDomain: authDomain,
+      databaseURL: databaseURL,
+      projectId: projectId,
+      storageBucket: storageBucket,
+      messagingSenderId: messagingSenderId,
+      appId: appId,
+      measurementId:measurementId
+    };
+    
+    
+    // Initialize Firebase
+    
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  
+  //Look into merge true.
+ // firebase.firestore().settings({ experimentalForceLongPolling: true,merge: true });
+
+
+
+  const reference = firebase
+  .app()
+  .database(databaseURL)
+  .ref('/users/123');
+  useEffect(()=>{
+    console.log("Testing");
+    const onValueChange = database()
+      .ref(`/Temperature2/`)
+      .on('value', snapshot => {
+        console.log('User data: ', snapshot.val());
+      });
+   // test();
+   /**  async function test() {
+      let example = "Hello World";
+      await storeData(example);
+      await getData();  
+    } **/
+    
+  },[])
+//https://react-native-async-storage.github.io/async-storage/docs/limits/
+  const storeData = async (value : any) => {
+    try {
+      await AsyncStorage.setItem('@storage_Key', value)
+    } catch (e) {
+      // saving error
+    }
+  }
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@storage_Key')
+      console.log(value);
+      if(value !== null) {
+        // value previously stored
+      }
+    } catch(e) {
+      // error reading value
+    }
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar/>
